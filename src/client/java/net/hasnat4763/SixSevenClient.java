6c3eccc819101b1bc2d87c2a783361b5fb1206ae
Hasnat4763 <abulhasnat5737@gmail.com>
2025-10-30 20:28:54 +0600

    Initial commit

---
+++ src/client/java/net/hasnat4763/SixSevenClient.java
@@ -0,0 +1,10 @@
+package net.hasnat4763;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SixSevenClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
