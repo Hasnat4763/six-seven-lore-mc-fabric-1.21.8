07ef9985da0f4c3965e88a0925f6514e50e42223
Hasnat4763 <abulhasnat5737@gmail.com>
2025-10-31 22:16:28 +0600

    Fixed some problems with Book GUI

--- src/client/java/net/hasnat4763/SixSevenStoryBookScreen.java
+++ src/client/java/net/hasnat4763/SixSevenStoryBookScreen.java
@@ -4,20 +4,21 @@ import net.hasnat4763.ScreenHandler.SixSevenStoryBookScreenHandler;
 import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-
 import static net.hasnat4763.SixSeven.MOD_ID;
 
 public class SixSevenStoryBookScreen extends HandledScreen<SixSevenStoryBookScreenHandler> {
 
     private static final Identifier TEXTURE = Identifier.of(MOD_ID, "textures/gui/six_seven.png");
+    private static final String TOTAL_PAGES = "10";
 
     public SixSevenStoryBookScreen(SixSevenStoryBookScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
-        this.backgroundWidth = 512;
-        this.backgroundHeight = 256;
+        this.backgroundWidth = 256;
+        this.backgroundHeight = 180;
         SixSeven.LOGGER.info("Loading texture: {}", TEXTURE);
     }
 
@@ -25,17 +26,36 @@ public class SixSevenStoryBookScreen extends HandledScreen<SixSevenStoryBookScre
     protected void init() {
         super.init();
         this.titleX = (this.backgroundWidth - this.textRenderer.getWidth(this.title)) / 2;
+
+        this.addDrawableChild(ButtonWidget.builder(Text.literal("< Previous"), button -> {
+            if (handler.getCurrentPage() > 0) {
+                handler.previousPage();
+            }
+        }).dimensions(x + 10, y + backgroundHeight - 30, 50, 20). build());
+        this.addDrawableChild(ButtonWidget.builder(Text.literal("Next >"), button -> {
+            if (handler.getCurrentPage() < 9) {
+                handler.nextPage();
+            }
+        }).dimensions(x + backgroundWidth - 60, y + backgroundHeight - 30, 50, 20). build());
+
     }
+
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         this.renderBackground(context, mouseX, mouseY, delta);
         super.render(context, mouseX, mouseY, delta);
         this.drawMouseoverTooltip(context, mouseX, mouseY);
+
         int x = (this.width - this.backgroundWidth) / 2;
         int y = (this.height - this.backgroundHeight) / 2;
-        Text pageText = Text.literal("Page " + (handler.getCurrentPage() + 1) + " / 67");
-        context.drawText(this.textRenderer, pageText, x + 10, y + 10, 0xFF000000, false);
+
+        Text pageText = Text.literal("Page " + (handler.getCurrentPage() + 1) + " / " + TOTAL_PAGES);
+        int pageTextWidth = this.textRenderer.getWidth(pageText);
+        context.drawText(this.textRenderer, pageText,
+                x + (backgroundWidth - pageTextWidth) / 2,
+                y + backgroundHeight - 15,
+                0xFF404040, false);
     }
 
     @Override
@@ -54,7 +74,11 @@ public class SixSevenStoryBookScreen extends HandledScreen<SixSevenStoryBookScre
                 0, 0,
                 this.backgroundWidth,
                 this.backgroundHeight,
-                512, 256
+                256, 180
         );
+    }
+    @Override
+    protected void handledScreenTick() {
+        super.handledScreenTick();
     }
 }
--- src/main/java/net/hasnat4763/ScreenHandler/SixSevenStoryBookScreenHandler.java
+++ src/main/java/net/hasnat4763/ScreenHandler/SixSevenStoryBookScreenHandler.java
@@ -6,24 +6,30 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.screen.ArrayPropertyDelegate;
+import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.Hand;
 
 public class SixSevenStoryBookScreenHandler extends ScreenHandler {
     private final ItemStack bookStack;
-    private int currentPage = 0;
-    private static final int TOTAL_PAGES = 20;
+    private static final int TOTAL_PAGES = 10;
+    private final PropertyDelegate propertyDelegate;
 
     public SixSevenStoryBookScreenHandler(int syncId, PlayerInventory playerInventory, Hand hand) {
         super(ModScreenHandler.SIX_SEVEN_STORY_BOOK_HANDLER, syncId);
         this.bookStack = playerInventory.player.getStackInHand(hand);
+        this.propertyDelegate = new ArrayPropertyDelegate(1);
+        this.addProperties(propertyDelegate);
     }
 
     public void nextPage() {
         NbtComponent customData = bookStack.get(DataComponentTypes.CUSTOM_DATA);
         NbtCompound nbt = customData != null ? customData.copyNbt() : new NbtCompound();
-
-        currentPage++;
+        int currentPage = propertyDelegate.get(0);
+        if (currentPage < TOTAL_PAGES) {
+            propertyDelegate.set(0, currentPage + 1);
+        }
         if (currentPage >= TOTAL_PAGES) {
             if (!nbt.contains("six_seven_read_complete")) {
                 nbt.putBoolean("six_seven_read_complete", true);
@@ -32,8 +38,15 @@ public class SixSevenStoryBookScreenHandler extends ScreenHandler {
         }
     }
 
+    public void previousPage() {
+        int currentPage = propertyDelegate.get(0);
+        if (currentPage > 0) {
+            propertyDelegate.set(0, currentPage - 1);
+        }
+    }
+
     public int getCurrentPage() {
-        return this.currentPage;
+        return propertyDelegate.get(0);
     }
 
     public ItemStack getBookStack() {
